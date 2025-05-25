7658183d8f8e9279c541f54a5a4e2822a93962ac
umayerCoding1 <umayerpro175@gmail.com>
2025-05-25 20:32:10 +0600

    feat: Add Vercel configuration file for deployment setup

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://carsetgo.vercel.app"
     ],   
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
