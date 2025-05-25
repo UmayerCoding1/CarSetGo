2324b20b089e122dac6b94cd71bdb83a88bbfcd9
umayerCoding1 <umayerpro175@gmail.com>
2025-05-25 21:09:25 +0600

    feat: Add Vercel configuration file for deployment setup

--- server/middlewares/verify.user.js
+++ server/middlewares/verify.user.js
@@ -2,7 +2,8 @@ import jwt, { decode } from "jsonwebtoken";
 
 const verifyUser = async (req, res, next) => {
   const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
- 
+  console.log(token);
+  
   
   if (!token)
     return res
