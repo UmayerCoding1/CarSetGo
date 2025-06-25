2997dd41ab8a42ae8040fff42cea3f082b6b309f
umayerCoding1 <umayerpro175@gmail.com>
2025-06-26 01:05:06 +0600

    Add search functionality for dealership requests and enhance layout

--- client/src/pages/seller/dealership/delalership-request/DealershipRequest.jsx
+++ client/src/pages/seller/dealership/delalership-request/DealershipRequest.jsx
@@ -6,12 +6,18 @@ import Table from "../../../../components/ui/table/Table";
 import DealershipTavleRow from "../../../../components/seller/DealershipTavleRow";
 import Loading from "../../../../components/ui/Loading";
 import { toast } from "sonner";
+import { Search } from "lucide-react";
+import { useState } from "react";
 
 /* 
  1: todo: create recovary section for dealearship request
+ 2: todo: implement pegination
 */
 
 const DealershipRequest = () => {
+ const [dealershipTranIdSearch, setDealershipTranIdSearch] = useState('');
+ const [searchInput,setSearchInput] = useState('');
+
   const { user } = useAuth();
   const { data: dealershipRequests = [], refetch: requestRefetch } = useQuery({
     queryKey: ["dealershipRequests"],
@@ -36,35 +42,54 @@ const DealershipRequest = () => {
     }
   };
 
+
+  console.log(dealershipTranIdSearch);
+  
   if (!dealershipRequests && !dealershipRequests?.length) {
     return <Loading />;
   }
 
   return (
     <div className="p-2">
-
+      <h1 className="text-2xl font-bold">Dealership Request</h1>
+      <div className="p-3 lg:flex items-center justify-between ">
+        <form onSubmit={(e) => {e.preventDefault()
+          setDealershipTranIdSearch(searchInput);
+        }}  className=" w-full lg:w-96 border border-gray-400 rounded-lg flex items-center p-2">
+          <input type="text" onChange={(e) => setSearchInput(e.target.value)} placeholder="Search form transaction Id" className="flex-1 h-10 outline-none" />
+          <button  className="bg-black text-white h-10 w-10 flex items-center justify-center rounded-xl">
+             <Search size={15}/>
+          </button>
+        </form>
         <div>
-            k
+          s
         </div>
-      <Table
-        thValue={[
-          "TransactionId",
-          "Name",
-          "Email",
-          "Phone",
-          "amount",
-          "Payment date",
-          "buyer",
-          "payment",
-          "Status",
-          "Action",
-        ]}
-      >
-        <DealershipTavleRow
-          requestsData={dealershipRequests.validDealership}
-          handleDealershipStatusChange={handleDealershipStatusChange}
-        />
-      </Table>
+      </div>
+
+
+
+
+      <div className="overflow-x-auto">
+        <Table
+          thValue={[
+            "TransactionId",
+            "Name",
+            "Email",
+            "Phone",
+            "amount",
+            "Payment date",
+            "buyer",
+            "payment",
+            "Status",
+            "Action",
+          ]}
+        >
+          <DealershipTavleRow
+            requestsData={dealershipRequests.validDealership}
+            handleDealershipStatusChange={handleDealershipStatusChange}
+          />
+        </Table>
+      </div>
     </div>
   );
 };
